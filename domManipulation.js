454dfc80d263204a6bc7cb45d901c2c6b9150354
Lucai994 <luca.muratori.94@hotmail.it>
2021-12-09 16:55:09 +0100

    create a function that highlight the number once is selected

--- domManipulation.js
+++ domManipulation.js
@@ -14,6 +14,10 @@ const createNumbers =function(){
         //we add a class in every div
         newNumberNode.classList.add('number')
         numberContainerNode.appendChild(newNumberNode)
+
+        //create a function as a listener for the click event
+        newNumberNode.onclick=selectNumber
+        numberContainerNode.appendChild(newNumberNode)
     }
 
 }
@@ -21,11 +25,29 @@ const createNumbers =function(){
 window.onload = function() {
     
     createNumbers()
+    
 }
 
 //create a button that randomize a number from 1 to 76
 const generateRandomNumber= function(){
-    const randomNumber= Math.floor(Math.random()*77)
+    const randomNumber= Math.floor((Math.random()*76)+1)
     let randomNumberNode=document.getElementById('random-number')
     randomNumberNode.textContent=randomNumber
+}
+
+
+//create a function that highlight the number generate from the random number generator
+
+const getCurrentlySelectedDay = function() {
+    return document.querySelector(".selected")
+}
+
+const selectNumber= function(e){
+    const currentlyNumberSelected=getCurrentlySelectedDay()
+    if(currentlyNumberSelected!==null){
+        currentlyNumberSelected.classList.remove('selected')
+    }
+    const clickedNumberNode = e.target
+    clickedNumberNode.classList.add('selected')
+
 }
